f532a8f23de6e2b8ce7fa32b66bf044fe234d194
raffiuddind <153497429+raffiuddind@users.noreply.github.com>
2026-03-26 22:11:58 +0530

    Create WaheedFruitTraderApplication.java with Spring Boot main application class

---
+++ src/main/java/com/waheedfruittrader/WaheedFruitTraderApplication.java
@@ -0,0 +1,12 @@
+package com.waheedfruittrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaheedFruitTraderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaheedFruitTraderApplication.class, args);
+    }
+}
